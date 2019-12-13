e5b5c6c76dde50425c8949011422badc4571e226
Avinash Nepal <nepalavinash@gmail.com>
2019-12-13 14:08:45 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+if (i==1){
+array.push(`I am ${i} strange loop.`);
+}
+else {array.push(`I am ${i} strange loops.`)}
+  }
+  return array ;
+}
+function whileLoop(n) {
+
+  while (n>0) {
+    console.log(n)
+    n =  n - 1
+  }
+  return 'done'
+}
